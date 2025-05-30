611557a556c3ee02c514c2d0346b834eab61723b
Yauheni Shumsky <shumsky.yauheni@gmail.com>
2025-05-30 03:20:17 +0300

    add ToString

--- Task3/Dice.cs
+++ Task3/Dice.cs
@@ -3,4 +3,13 @@
 public class Dice(List<string> sides)
 {
     public List<int> Sides { get; } = sides.Select(int.Parse).ToList();
+
+    public override string ToString()
+    {
+        var response = string.Empty;
+        response = "[";
+        response += string.Join(", ", Sides);
+        response += "]";
+        return response;
+    }
 }
